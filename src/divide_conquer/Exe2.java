94dc56be08987427fa52d101eaded7ad9ff64a3e
Frank_Xiao <Xiao670127@126.com>
2023-05-27 14:39:00 +0800

    modify javadoc

--- src/divide_conquer/Exe2.java
+++ src/divide_conquer/Exe2.java
@@ -2,6 +2,13 @@ package divide_conquer;
 
 /**
  * Exe2 is for exercise2.
+ * <p>
+ * The question is as follows:<br>
+ * 给定一个有n（n≥1）个整数的序列，要求求出其中最大连续子序列的和。（分治策略求解， Java or Python）<br>
+ * 例如：<br>
+ * 序列（-2，11，-4，13，-5，-2）的最大子序列和为20<br>
+ * 序列（-6，2，4，-7，5，3，2，-1，6，-9，10，-2）的最大子序列和为16。<br>
+ * 规定一个序列最大连续子序列和至少是0，如果小于0，其结果为0。<br>
  */
 public class Exe2 {
     /**
