ad5996a58a94e0e103c0c714d69928d4268d44ce
Frank_Xiao <Xiao670127@126.com>
2023-05-27 14:18:04 +0800

    modify Exe2.MaxSubSum javadoc

--- src/divide_conquer/Exe2.java
+++ src/divide_conquer/Exe2.java
@@ -1,6 +1,12 @@
 package divide_conquer;
 
 public class Exe2 {
+    /**
+     * Find the maximum sum of an array's sub-array.
+     *
+     * @param arr the array to be found
+     * @return the maximum sum of the array's sub-array
+     */
     public static int MaxSubSum(int[] arr) {
         int sum = 0;
 
