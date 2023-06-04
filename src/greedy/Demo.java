f0472c4a0f077aa50a961b3c0aefdfe4078d8112
Frank_Xiao <Xiao670127@126.com>
2023-06-04 19:37:48 +0800

    xxj

--- src/greedy/Demo.java
+++ src/greedy/Demo.java
@@ -1,5 +1,7 @@
 package greedy;
 
+import data_structure.UndirectedAcyclicGraph;
+
 import java.util.HashMap;
 
 /**
@@ -10,20 +12,20 @@ import java.util.HashMap;
  */
 public class Demo {
     public static void main(String[] args) {
-//        UndirectedAcyclicGraph graph = new UndirectedAcyclicGraph(8);
-//        graph.addEdge(3, 1, 5);
-//        graph.addEdge(1, 2, 3);
-//        graph.addEdge(3, 4, 12);
-//        graph.addEdge(2, 4, 4);
-//        graph.addEdge(4, 5, 9);
-//        graph.addEdge(4, 8, 8);
-//        graph.addEdge(5, 8, 1);
-//        graph.addEdge(6, 5, 4);
-//        graph.addEdge(6, 7, 6);
-//        graph.addEdge(5, 7, 5);
-//        graph.addEdge(7, 8, 20);
-//        Exe1.Kruskal(graph);
-//        Exe1.Dijkstra(graph, 4);
+        UndirectedAcyclicGraph graph = new UndirectedAcyclicGraph(8);
+        graph.addEdge(3, 1, 5);
+        graph.addEdge(1, 2, 3);
+        graph.addEdge(3, 4, 12);
+        graph.addEdge(2, 4, 4);
+        graph.addEdge(4, 5, 9);
+        graph.addEdge(4, 8, 8);
+        graph.addEdge(5, 8, 1);
+        graph.addEdge(6, 5, 4);
+        graph.addEdge(6, 7, 6);
+        graph.addEdge(5, 7, 5);
+        graph.addEdge(7, 8, 20);
+        Exe1.Kruskal(graph);
+        Exe1.Dijkstra(graph, 4);
 
         HashMap<Character, Integer> map = new HashMap<>();
         map.put('a', 12);
@@ -33,18 +35,12 @@ public class Demo {
         map.put('e', 14);
         map.put('f', 85);
         HashMap<Character, String> result = Exe1.Huffman(map);
-        map.clear();
-        map.put('*', 5);
-        map.put('w', 4);
-        map.put('l', 4);
-        map.put('e', 2);
-        map.put('i', 2);
-        map.put('r', 1);
-        map.put('u', 1);
-
-        result = Exe1.Huffman(map);
+        System.out.println("Huffman code:");
         for (char c : result.keySet())
             System.out.println(c + "--" + result.get(c));
+
+        double value = Exe2.Alibaba(new int[]{50, 60, 100, 120, 45}, new int[]{4, 10, 20, 30, 15});
+        System.out.println("Alibaba maximum value = " + value);
 
     }
 }
--- src/greedy/Exe2.java
+++ src/greedy/Exe2.java
@@ -1,16 +1,49 @@
 package greedy;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * Exe2 is for exercise2.
  * <p>
  * The question is as follows:<br>
- * 阿里巴巴走进了装满宝藏的藏宝洞。
- * 藏宝洞里面有 N（N≤100）堆金币，第i堆金币的总重量和总价值分别为mi和vi（1≤mi，vi≤100）。
- * 阿里巴巴有一个承重力为T（T≤1000）的背包，但并没办法将全部的金币都装进去。他想装走尽可能多价值的金币。
+ * 阿里巴巴走进了装满宝藏的藏宝洞。<br>
+ * 藏宝洞里面有 N（N≤100）堆金币，第i堆金币的总重量和总价值分别为mi和vi（1≤mi，vi≤100）。<br>
+ * 阿里巴巴有一个承重力为T（T≤1000）的背包，但并没办法将全部的金币都装进去。他想装走尽可能多价值的金币。<br>
  * 所有金币都可以随意分割，分割完的金币重量比（单位价格）不变，请问阿里巴巴最多可以拿走多少价值金币？<br>
  */
 public class Exe2 {
-    public static int Alibaba(int N) {
-
+    /**
+     * Solve the problem Exercise 2 in greedy part.
+     *
+     * @param m an array to tell the mass of each heap
+     * @param v an array to tell the value of each heap
+     * @return the maximum value of the bag
+     */
+    public static double Alibaba(int[] m, int[] v) {
+        if (m.length != v.length) throw new AssertionError();
+        ArrayList<Double> vpm = new ArrayList<>();
+        for (int i = 0; i < m.length; i++)
+            vpm.add((double) v[i] / m[i]);
+        Comparator<Double> comparator = new Comparator<>() {
+            @Override
+            public int compare(Double o1, Double o2) {
+                if (o1 < o2)
+                    return -1;
+                else if (o1 == o2)
+                    return 0;
+                else return 1;
+            }
+        };
+        double remain = 1000, totalValue = 0.0;
+        while (remain > 0 && vpm.size() > 0) {
+            Double maxVPM = vpm.stream().max(comparator).get();
+            int index = vpm.indexOf(maxVPM);
+            double used = Math.min(remain, m[index]);
+            remain -= used;
+            totalValue += used * maxVPM;
+            vpm.remove(maxVPM);
+        }
+        return totalValue;
     }
 }
